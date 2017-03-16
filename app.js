1cdc64ab2dca54d65101ec96f77a4d5c68b0c4aa
josselin chevalay <josselin54.chevalay@gmail.com>
2017-03-16 19:54:55 +0100

    jshint a bit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/** jshint es6 */
 var express = require('express');
 var config  = require('./config');
 var app = express();
@@ -38,10 +38,11 @@ var authorization = new passportLocal(passport, config, logger);
 app.use(authorization.initialize());
 app.use(authorization.session());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, accessToken");
-  next();
+app.use((req, res, next) => {
+	'use strict';
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, accessToken");
+    next();
 });
 
 app.get('/auth/facebook', authorization.authenticate('facebook', { scope : ['email', 'public_profile', 'user_posts'] }));
@@ -54,100 +55,105 @@ app.get('/auth/facebook/callback',
 }));
 
 function isLogged(req, res, next){
-	var db = mongoose.createConnection(config.db.connectionString); // to be carefull with this
-    
-    db.on('error', function(err){
-            logger.error('mongo '+err);
-            mongoose.connection.close();
-            return;
+    'use strict';
+    var db = mongoose.createConnection(config.db.connectionString); // to be carefull with this
+    db.on('error', (err) => {
+        logger.error('mongo ' + JSON.stringify(err));
+        mongoose.connection.close();
+        return;
     });
-    if(req.user){
-    	logger.info("user logged "+req.user.userName);
-    	next();
+    if (req.user){
+        logger.info("user logged " + req.user.userName);
+        next();
     }
     var accessToken = req.cookies.accessToken || req.headers.accesstoken;
-    if(!accessToken){
-    	logger.error('request invalid : '+req);
-    	res.send(401);
-    	return;
+    if (!accessToken){
+        logger.error('request invalid : ' + JSON.stringify(req));
+        res.send(401);
+        return;
     }
- 	logger.info('third party use token :'+accessToken); 	
- 	db.once('open', function(){
- 		var U = db.model('User');
- 		U.find({token:accessToken}, function(err, docs){
- 			if(docs.length > 1){
- 				res.send(409)
- 				logger.error('conflict for the token');
- 				db.close();
- 				return false;
- 			}
- 			logger.info('found '+ docs);
- 			req.user = docs[0];
- 			db.close();
- 			next();
- 		});
- 	});
+    logger.info('third party use token :' + accessToken); 	
+    db.once('open', () => {
+        var U = db.model('User');
+        U.find({token: accessToken}, (err, docs) =>{
+            if (docs.length > 1) {
+                res.send(409);
+                logger.error('conflict for the token');
+                db.close();
+                return false;
+            }
+            logger.info('found ' + docs);
+            req.user = docs[0];
+            db.close();
+            next();
+       });
+     });
 }
 
 /**
 * general route
 */
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', (req, res) => {
+    'use strict';
+     res.send('Hello World!');
 });
 
-app.get('/logout', function(req, res){
-	if(req.user){
-		logger.info('disconnect '+req.user.userName);
-		req.session.destroy();
-		req.logout();
-	}	
+app.get('/logout', (req, res) => {
+	'use strict';
+    if (req.user) {
+        logger.info('disconnect ' + req.user.userName);
+        req.session.destroy();
+        req.logout();
+    }	
     res.redirect('/');
 });
 
-app.get('/logged', isLogged, function(req, res){
-	logger.info('logged '+req.user.userName);
-    res.redirect(config.frontendURl+req.user.token);
+app.get('/logged', isLogged, (req, res) => {
+    'use strict';
+    logger.info('logged ' + req.user.userName);
+    res.redirect(config.frontendURl + req.user.token);
 });
 
 /*
 * IMPORTANT PRIVATE route
 */
-app.get('/authorize', isLogged, function(req, res){
-	if(!req.user){
-		res.send(401);
-	}
-	res.json(req.user);
+app.get('/authorize', isLogged, (req, res) => {
+    'use strict';
+    if (!req.user) {
+        res.send(401);
+    }
+    res.json(req.user);
 });
 
 /**
 * IMPORTANT PUBLIC ROUTE
 */
-app.get('/whois/:uid', function(req, res){
-	if(!req.params['uid']){
-		res.send(404);
-	}
-	mongoose.connect(config.db.connectionString);
+app.get('/whois/:uid', (req, res) => {
+    'use strict';
+    if (!req.params['uid']) {
+        res.send(404);
+    }
+    mongoose.connect(config.db.connectionString);
     var db= mongoose.connection;
-    db.on('error', function(err){
-        logger.error('mongo '+err);
+    db.on('error', (err) => {
+        logger.error('mongo ' + JSON.stringify(err));
         mongoose.connection.close();
         done(err);
     });
-    db.once('open', function(){
-    	User.findById(req.params['uid'], function(err, doc){
-    		if(err || !doc){
-    			logger.error("whois route not found user for : "+req.params['uid']);
-    			mongoose.connection.close();
-    			res.send(404);
-    			return;
-    		}
-    		mongoose.connection.close();
-    		var usr = doc.toJSON();
-    		var usrClean = _.omit(usr, config.privacyColumns);
-    		logger.info(usrClean);
-    		res.json(usrClean);
-    	});
+    db.once('open', () => {
+        User.findById(req.params['uid'], function(err, doc){
+            if (err || !doc) {
+                logger.error("whois route not found user for : "+req.params['uid']);
+                mongoose.connection.close();
+                res.send(404);
+                return;
+            }
+            mongoose.connection.close();
+            var usr = doc.toJSON();
+            var usrClean = _.omit(usr, config.privacyColumns);
+            logger.info(usrClean);
+            res.json(usrClean);
+       });
     });
 });
 
@@ -155,5 +161,6 @@ app.get('/whois/:uid', function(req, res){
 
 
 app.listen(config.server.port, function () {
-  logger.info('server '+config.server.port);
+    'use strict';
+    logger.info('server ' + config.server.port);
 });
--- services/logger/index.js
+++ services/logger/index.js
@@ -1,12 +1,11 @@
-var log4js		 = require('log4js');
+var log4js = require('log4js')
 
-module.exports = function(config){
-	log4js.configure({
-	  appenders: [
-	    { 'type': 'console' },
-	    { 'type': 'file', 'filename': config.logging.file, 'maxLogSize': 20480, 'backups': 10, }
-	  ]
-	});
-
-	return log4js.getLogger(config.logging.category);
+module.exports = function (config) {
+    'use strict';
+    log4js.configure({
+        appenders: [{'type': 'console' },
+                   {'type': 'file', 'filename': config.logging.file, 'maxLogSize': 20480, 'backups': 10}
+        ]
+    });
+    return log4js.getLogger(config.logging.category);
 };
